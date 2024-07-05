55d7909f320b18751317c2ad85c69fdb179e6c77
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-05 00:22:13 -0500

    completed seed function

--- seed.ts
+++ seed.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import csv from 'csv-parser'
 
 interface Row {
     text: string
@@ -9,5 +10,15 @@ async function parseCSV(filePath: string): Promise<Row[]> {
         const rows: Row[] = []
 
         fs.createReadStream(filePath)
+            .pipe(csv({separator: ","}))
+            .on("data", (row) => {
+                rows.push(row)
+            })
+            .on("error", (err) => {
+                reject(err)
+            })
+            .on("end", () => {
+                resolve(rows)
+            })
     })
 }
