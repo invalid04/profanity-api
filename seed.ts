21a4d0b7358ad56403465ea2dfa55779da13bdd0
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-05 00:19:13 -0500

    testing connection

--- seed.ts
+++ seed.ts
@@ -4,10 +4,10 @@ interface Row {
     text: string
 }
 
-async function parseCSV(filepath: string): Promise<Row[]> {
+async function parseCSV(filePath: string): Promise<Row[]> {
     return new Promise((resolve, reject) => {
         const rows: Row[] = []
 
-
+        fs.createReadStream(filePath)
     })
 }
