fc524835ac7c69f071da05b25ddb2b140d326a3c
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-11 00:46:39 -0500

    completed splitTextIntoSemantics function

--- index.ts
+++ index.ts
@@ -69,5 +69,7 @@ function splitTextIntoWords(text: string) {
 
 async function splitTextIntoSemantics(text: string) {
     if(text.split(/\s/).length === 1) return []
-
+    const documents = await semanticSplitter.createDocuments([text])
+    const chunks = documents.map((chunk) => chunk.pageContent)
+    return chunks
 }
